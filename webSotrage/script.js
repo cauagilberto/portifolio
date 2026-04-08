e44be5f30d2224ec5f97a08f4719ebce07bf92c5
Caua Gilberto <cauagilbertop@outlook.com>
2026-04-08 18:59:45 -0300

    usando webStorage para alterar tema da aba

--- webSotrage/script.js
+++ webSotrage/script.js
@@ -1,1 +1,32 @@
+const botaoTema = document.getElementById("botaoTema");
 
+botaoTema.addEventListener("click", () => {
+    //verficar se o usuario ja tem tema
+    const temaAtual = localStorage.getItem("tema");
+    const temaNovo = temaAtual === "dark" ? "light" : "dark";
+
+    //verificar qual o tema atual
+    document.body.classList.remove("dark", "light");
+    document.body.classList.add(temaNovo);
+
+    //salvar tema no localStorage
+    localStorage.setItem("tema", temaNovo);
+
+    //atualizar o texto do botão
+    botaoTema.textContent = temaNovo === "dark" ? "*" : "/";
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    //verificar se o usuario ja tem tema salvo
+    const temaSalvo = localStorage.getItem("tema");
+
+    //se for dark adiciona a classe e altera o botão
+    if (temaSalvo === "dark") {
+        document.body.classList.add("dark");
+        botaoTema.textContent = "*";
+    } else {
+        document.body.classList.add("light");
+        botaoTema.textContent = "/";
+    }
+});
