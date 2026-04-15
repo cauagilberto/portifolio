699ae9c1c868344afd0a989287d90ac4f04c31de
Caua Gilberto <cauagilbertop@outlook.com>
2026-04-14 22:54:44 -0300

    o crudcrud morreu, adicionar tarefa atraves do input

--- metodoHTTP/script.js
+++ metodoHTTP/script.js
@@ -14,4 +14,30 @@ fetch("https://crudcrud.com/api/472f20fad5e04b0f99ada92bac78a6a0/tarefas")
         //adiciona novo item a lista
         tarefas.appendChild(item);
     });
+});
+
+//fazendo post atraves da aba do navegador
+document.getElementById('add').addEventListener('click', () => {
+    //pega descrição do input do usuario
+    const descricao = document.getElementById('tarefa').value;
+    //faz um POST para a API para criar uma nova tarefa
+    fetch("https://crudcrud.com/api/472f20fad5e04b0f99ada92bac78a6a0/tarefas", {
+        //definindo como POST, mas poderia ser PUT, DELETE, etc
+        method: "POST",
+        //definindo o tipo de conteúdo como JSON
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ descricao: descricao }) //envia a descrição da tarefa como JSON
+    })
+    .then(response => response.json()) //converte a resposta para JSON
+    .then((tarefa) => {
+        //cria um elemento li para cada tarefa
+        const item = document.createElement('li');
+        //define o conteúdo do item com a descrição da tarefa e um botão de excluir
+        item.innerHTML = `${tarefa.descricao} <button>X</button>`;
+        //adiciona novo item a lista
+        tarefas.appendChild(item);
+    })
+
 })
