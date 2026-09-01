cb02f1eab5bbb723e3367afa6cb1c4589ab19a2f
Cauã Gilberto Paiva Lainha <cauagilbertop@outlook.com>
2026-09-01 17:12:14 -0300

    usando JavaScript para criar keys dos elementos e se adaptar ao React: Desktop(Linux)

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,16 @@
 import Tarefa from './components/Tarefa'
 
 function App() {
-    const tarefas = ["Estudar React", "Fazer compras", "Responder e-mails"]
+    const tarefas = [
+        {id: 1, texto: "Estudar React"},
+        {id: 2, texto: "Fazer compras"},
+        {id: 3, texto: "Responder e-mails"},
+    ];
     return (
         <main>
             <h1>To-Do List App</h1>
                 <ul>
-                    {tarefas.map(tarefa => <Tarefa texto={tarefa}/>)}
+                    {tarefas.map(tarefa => <Tarefa key={tarefa.id} texto={tarefa.texto}/>)}
                 </ul>
         </main>
     )
